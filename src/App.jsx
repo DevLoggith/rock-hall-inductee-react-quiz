3cbe619ded8f4d4e78bb7beccc8190c0f87062a9
logan <92327020+DevLoggith@users.noreply.github.com>
2026-03-21 19:41:45 -0400

    add inductee data and import to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import inductees from './data/inductees.json'
 import './App.css'
 
 function App() {
+  console.log(inductees)
   const [count, setCount] = useState(0)
 
   return (
