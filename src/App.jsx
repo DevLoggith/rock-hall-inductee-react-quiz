d5839e4d9057298a6e181772e50b779d0f82d8df
logan <92327020+DevLoggith@users.noreply.github.com>
2026-03-23 18:56:46 -0400

    create initial game states

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import inductees from './data/inductees.json'
 import './App.css'
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [askedInductees, setAskedInductees] = useState([]);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [questionsAnswered, setQuestionsAnswered] = useState(0);
 
   return (
     <>
