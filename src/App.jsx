47f7878edc18277219dcbdcd0ec726c3693c9f39
logan <92327020+DevLoggith@users.noreply.github.com>
2026-03-23 18:34:00 -0400

    remove predefined styles & App() logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,9 @@ import inductees from './data/inductees.json'
 import './App.css'
 
 function App() {
-  console.log(inductees)
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
     </>
   )
 }
