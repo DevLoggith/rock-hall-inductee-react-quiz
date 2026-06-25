851215a85e1bbc40ca65df0d80485063a4728059
logan <92327020+DevLoggith@users.noreply.github.com>
2026-06-25 19:17:26 -0400

    install libraries & update vite, vitest, & eslint configs

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import vitestGlobals from 'eslint-config-vitest-globals/flat'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,16 +15,15 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
       },
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
+  vitestGlobals(),
 ])
