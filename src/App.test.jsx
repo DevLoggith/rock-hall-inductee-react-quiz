1562f75c199a0d0fffbf0c86cefc04eb559c48c4
logan <92327020+DevLoggith@users.noreply.github.com>
2026-07-16 20:00:05 -0400

    create test suite and test case for correct answer logic

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,37 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { vi } from "vitest";
 
 import App from './App';
 
-describe('App', () => {
-  it('renders App component', () => {
-    render(<App />);
-  });
+const { mockQuestion } = vi.hoisted(() => {
+    return {
+        mockQuestion: {
+            inductee: { name: "Test Artist A", inductionYear: 1988, priorNominations: [1987], inductionPresenter: "Little Richard" },
+            answers: [1992, 2006, 1988, 1997],
+            correctAnswer: 1988
+        }
+    };
+});
+
+vi.mock("./utils/generateQuestion", () => ({
+    generateQuestion: vi.fn(() => mockQuestion)
+}));
+
+describe("QuestionCard", () => {
+    it("renders the correct answer as correct", async () => {
+        render(<App />);
+
+        const user = userEvent.setup();
+        const correctButton = screen.getByText("1988");
+
+        await user.click(correctButton);
+
+        const heading = screen.getByRole("heading", { level: 2 });
+        const headingSpan = within(heading).getByText("That's correct!")
+        
+
+        expect(headingSpan).toBeInTheDocument();
+        expect(correctButton).toHaveClass("correct");
+    });
 });
