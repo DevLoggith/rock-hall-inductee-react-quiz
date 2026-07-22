be15bbeaf0ad801e5a10266b07888c9cd31de423
logan <92327020+DevLoggith@users.noreply.github.com>
2026-07-21 22:27:23 -0400

    abstract score screen logic into it's own component
    when trying to create an integration test for rendering the score screen,
    i realized it would not only be easier to test, but also declutter the
    App component while only needing to pass playAgain, correctAnswers, and
    totalQuestions as props

---
+++ src/components/ScoreScreen.jsx
@@ -0,0 +1,33 @@
+import PlayAgainButton from "./PlayAgainButton";
+
+function ScoreScreen({ correctAnswers, totalQuestions, playAgain }) {
+    const scorePercentage = (correctAnswers / totalQuestions) * 100;
+    const gameScoreText = (
+        <>
+            You got{" "}
+            <span className="artist-name">
+                {correctAnswers} out of {totalQuestions}
+            </span>{" "}
+            questions right
+        </>
+    );
+
+    let gameResponseMsg =
+        "The Rock Hall's history runs deep, but now you know a little more of it.";
+
+    if (scorePercentage >= 80) {
+        gameResponseMsg = "Front row knowledge! You've earned your place in the crowd.";
+    } else if (scorePercentage >= 60) {
+        gameResponseMsg = "You've got the foundation, just a few more deep cuts to learn.";
+    }
+
+    return (
+        <section>
+            <h2 className="score-text">{gameScoreText}</h2>
+            <p className="score-response">{gameResponseMsg}</p>
+            <PlayAgainButton onPlayAgainSelect={() => playAgain()} />
+        </section>
+	);
+}
+
+export default ScoreScreen;
